b715e153a2f3c9c6173d6f86d7ffa21369e15411
Sumika06 <mcd2376028@stu.o-hara.ac.jp>
2026-01-26 14:18:12 +0900

    [add] 選挙一覧表示

---
+++ backend/src/main/java/com/bteam/ovs/elections/controller/CommitteeElectionsController.java
@@ -0,0 +1,24 @@
+package com.bteam.ovs.elections.controller;
+
+import com.bteam.ovs.elections.controller.dto.ElectionResponse;
+import com.bteam.ovs.elections.service.ElectionCommitteeService;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/committee/elections")
+public class CommitteeElectionsController {
+
+    private final ElectionCommitteeService committeeService;
+
+    public CommitteeElectionsController(ElectionCommitteeService committeeService) {
+        this.committeeService = committeeService;
+    }
+
+    @GetMapping
+    public List<ElectionResponse> list() {
+        return committeeService.listElections();
+    }
+}
